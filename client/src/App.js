6d93cfcdbbf494b77a3d9f8f4b4940f0d8d8727a
Mahdi Hassai <hassanim430@gmail.com>
2024-08-06 15:15:47 +0430

    configure react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="text-3xl">
-      Learn React
-    </div>
+    <>
+      <div className="container">
+        <Outlet />
+      </div>
+    </>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App></App>}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
-
